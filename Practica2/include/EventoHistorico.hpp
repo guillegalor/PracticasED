a05e2f4f58a9bc6716bc34662df6b28c5b4943e9
Guillermo Galindo <guillegalor@gmail.com>
2016-10-28 00:28:04 +0200

    EventoHistorico.cpp completed, ready for test

--- Practica2/include/EventoHistorico.hpp
+++ Practica2/include/EventoHistorico.hpp
@@ -14,42 +14,40 @@ struct Fecha{
 typedef std::string Acontecimiento; //Llamamos acontecimiento a un string que va a contener su nombre
 
 class EventoHistorico{
+  private:
+    Fecha f;
+    std::vector<Acontecimiento> evento;
 
-private:
-  Fecha f;
-  std::vector<Acontecimiento> evento;
+    std::vector<Acontecimiento>::iterator busquedaAcontecimiento(Acontecimiento a);
 
-  std::vector<Acontecimiento>::iterator busquedaAcontecimiento(Acontecimiento a);
+  public:
+    //Constructores
+    EventoHistorico();
+    EventoHistorico(int f);
+    EventoHistorico(Fecha f);
+    EventoHistorico(int f, std::vector<Acontecimiento> v);
+    EventoHistorico(Fecha f, std::vector<Acontecimiento> v);
 
-public:
-  //Constructores
-  EventoHistorico();
-  EventoHistorico(int f);
-  EventoHistorico(Fecha f);
-  EventoHistorico(int f, std::vector<Acontecimiento> v);
-  EventoHistorico(Fecha f, std::vector<Acontecimiento> v);
+    //Set Get
+    Fecha getFecha() const{return f;}
+    std::vector<Acontecimiento> getEvento() const{return evento;};
+    void setFecha(int f);
+    void setFecha(Fecha f);
+    void setEvento(std::vector<Acontecimiento> v);
+    void addEvento(Acontecimiento a);
+    void addEvento(std::vector<Acontecimiento> evento);
 
-  //Set Get
-  Fecha getFecha() const{return f;}
-  std::vector<Acontecimiento> getEvento() const{return evento;};
-  void setFecha(int f);
-  void setFecha(Fecha f);
-  void setEvento(std::vector<Acontecimiento> v);
-  void addEvento(Acontecimiento a);
-  void addEvento(std::vector<Acontecimiento> evento);
+    //Eliminar
+    bool eliminarAcontecimiento(Acontecimiento a);
+    void eliminar(std::string key);
 
-  //Eliminar
-  bool eliminarAcontecimiento(Acontecimiento a);
-  bool eliminar(std::string key);
+    //Busqueda
+    std::vector<Acontecimiento> buscarAcontecimientos(std::string key) const; //Busca acontecimientos que contienen la palabra clave "key"
 
-  //Busqueda
-  std::vector<Acontecimiento> buscarAcontecimientos(std::string key) const; //Busca acontecimientos que contienen la palabra clave "key"
-
-  //Entrada Salida
-  std::ostream& mostrarEvento(std::ostream& os) const;
-  std::istream& leerEvento(std::istream& is);
-  std::ostream& prettyPrint(std::ostream& os) const;
-
+    //Entrada Salida
+    std::ostream& mostrarEvento(std::ostream& os) const;
+    std::istream& leerEvento(std::istream& is);
+    std::ostream& prettyPrint(std::ostream& os) const;
 };
 
 #endif
--- Practica2/src/EventoHistorico.cpp
+++ Practica2/src/EventoHistorico.cpp
@@ -42,10 +42,17 @@ void EventoHistorico::setEvento(std::vector<Acontecimiento> v){
   this->evento = v;
 }
 
-void EventoHistorico::addEvento(Acontecimiento a){
-  
+void EventoHistorico::addEvento(Acontecimiento a){ // TODO Devolver un bool o no?
+  if (busquedaAcontecimiento(a) == evento.end()) {
+    evento.push_back(a);
+  }
 }
 
+void EventoHistorico::addEvento(std::vector<Acontecimiento> evento){
+  for (std::vector<Acontecimiento>::iterator p = evento.begin(); p != evento.end(); ++p){
+    addEvento(*p);
+  }
+}
 
 /*
 Busqueda y borrado
@@ -63,3 +70,82 @@ std::vector<Acontecimiento>::iterator EventoHistorico::busquedaAcontecimiento(Ac
 
   return p;
 }
+
+std::vector<Acontecimiento> EventoHistorico::buscarAcontecimientos(std::string key) const{
+  std::vector<Acontecimiento> v;
+
+  for (std::vector<Acontecimiento>::const_iterator p = evento.begin(); p != evento.end(); ++p){
+    if (p->find(key) != std::string::npos) {
+      v.push_back(*p);
+    }
+  }
+
+  return v;
+}
+
+bool EventoHistorico::eliminarAcontecimiento(Acontecimiento a){
+  if (busquedaAcontecimiento(a) != evento.end()) {
+    evento.erase(busquedaAcontecimiento(a));
+
+    return true;
+  }
+  else
+    return false;
+}
+
+void EventoHistorico::eliminar(std::string key){
+  std::vector<Acontecimiento>::iterator p = evento.begin();
+
+  while (p != evento.end()) {
+    if (p->find(key) != std::string::npos) {
+      evento.erase(p);
+    }
+    else
+      ++p;
+  }
+}
+
+/*
+E/S
+ */
+
+ std::ostream& EventoHistorico::mostrarEvento(std::ostream& os) const{
+   os << f.anio << '#' << f.dc;
+   for (std::vector<Acontecimiento>::const_iterator p = evento.begin(); p != evento.end(); ++p){
+     os << '#' << *p;
+   }
+
+   return os;
+ }
+
+ std::istream& EventoHistorico::leerEvento(std::istream& is){
+  std::string a;
+
+  std::getline(is, a, '#');
+  f.anio= std::stoi(a);
+
+  std::getline(is, a, '#');
+  f.dc= std::stoi(a);
+
+  while (std::getline(is, a, '#')) {
+    evento.push_back(a);
+  }
+
+  return is;
+ }
+
+ std::ostream& EventoHistorico::prettyPrint(std::ostream& os) const{
+  os << "Año: " << f.anio;
+  if (f.dc)
+    os << "dc";
+  else
+    os << "ac";
+
+  os << '\n';
+
+  for (std::vector<Acontecimiento>::const_iterator p = evento.begin(); p != evento.end(); ++p){
+    os << "  -"<< *p << std::endl;
+  }
+
+   return os;
+ }
